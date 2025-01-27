70d2de1b79a4592fc36429384a66df7e30236e25
Saad <saadsohailbajwa@gmail.com>
2025-01-27 08:52:12 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-blue-100 text-3xl'>todo app</h1>
+      <input type="text" placeholder='enter task name'></input>
+    </>
+  )
+}
+
+export default App
